29374d6446f65be621dc98bdec186935f2928f40
githubKushagra <118505260+githubKushagra@users.noreply.github.com>
2022-11-30 23:42:14 +0530

    Add files via upload
    my python mini project  is uploaded

---
+++ ca3 project INT.py
@@ -0,0 +1,50 @@
+#diceroll game
+
+import random
+score=0
+def rolldice(x):
+    global score
+    RN=random.randint(1,6)                                          
+    print("Dice roll : ",RN)
+    if int(x)==RN:
+        score+=5
+        print("_Congrats_You won")
+        print("You Got :",score,"points")
+    else:
+        print("_Sorry_You lose")
+        print("Total points scored :",score,"points")
+    WPA=input("Play one more match : ")                                 
+    if WPA in ["yes","y","Y","YES"]:
+        print("Yeah_Sure")
+        input_()
+    else:
+        print("Total points scored :",score)
+        print("ByeBye")
+
+def input_():
+    global a
+    a=input("Enter a number between '1-6' : ")
+    if len(a)==1:
+        if a.isdigit():
+            if int(a) in [1,2,3,4,5,6]:
+                 rolldice(a)
+            else:
+                print("!!Enter a number between '1-6' only!!")
+                input_()
+        else:
+            print("Enter a valid number")
+            input_()
+    else:
+
+        print("Enter only single digit number")
+        input_()
+        
+input_()
+
+
+
+
+
+
+
+
